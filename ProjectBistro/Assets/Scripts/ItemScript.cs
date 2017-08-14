d5f9e6eed4e8723ff2b648cfb43c815d45916dd7
chriskok <ckok@purdue.edu>
2017-08-14 12:03:46 -0400

    Updated item size and rotation

--- ProjectBistro/Assets/Scripts/ItemScript.cs
+++ ProjectBistro/Assets/Scripts/ItemScript.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 public class ItemScript : MonoBehaviour {
 
 	private Vector3 initialPos;
+	private Vector3 initialScale;
+	private Quaternion initialRot;
 
 	public GameObject model;
+	public Vector3 rotation;
+	public int sizeMultiplier;
 
 	// Use this for initialization
 	void Start () {
 		initialPos = transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		initialScale = transform.localScale;
+		initialRot = transform.rotation;
+
+		transform.localScale *= sizeMultiplier;
+		transform.rotation = Quaternion.Euler (rotation);
 	}
 
 	void OnMouseDrag(){
+		transform.localScale = initialScale;
+		transform.rotation = initialRot;
 		transform.position = new Vector3 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, transform.position.y , Camera.main.ScreenToWorldPoint (Input.mousePosition).z);
 	}
 
 	void OnMouseUp(){
 		transform.position = initialPos;
+		transform.localScale *= sizeMultiplier;
+		transform.rotation = Quaternion.Euler (rotation);
 
 		if (GameManager.mouseOver.x > 0) {
 			TileScript currentTile = GameManager.mapArray [(int)GameManager.mouseOver.x, (int)GameManager.mouseOver.y];
